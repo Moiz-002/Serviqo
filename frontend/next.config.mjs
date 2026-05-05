15601f6fc141961792be7d53e8e84bd1576c89f9
Moiz-002 <abdulmoizgd@gmail.com>
2026-05-05 11:55:31 +0500

    Frontend Update 11:55 AM TUE

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.1.6', '*.local'],
+  allowedDevOrigins: ['192.168.1.6', '*.local', '10.113.89.147'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
