05c380507d142c8fb81c1f4173048c93e8cdad3d
Moiz-002 <abdulmoizgd@gmail.com>
2026-06-20 13:48:14 +0500

    initial-commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.1.6', '*.local', '10.113.89.147'],
+  allowedDevOrigins: ['192.168.1.6', '*.local', '10.113.89.147', 'https://serviqo-21bb.onrender.com'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
